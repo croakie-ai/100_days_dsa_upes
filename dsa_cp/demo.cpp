797a58273e54b4a5e6a6e5e507f3c7988d192459
croakie-ai <yugal2316mahajan@gmail.com>
2026-07-13 23:39:18 +0530

    day 7 programmig task

---
+++ dsa_cp/demo.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int fib(int n){\
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    int a=0;
+    int b=1;
+    int c=0;
+    for(int i=1;i<n;i++){
+        c=a+b;
+        a=b;
+        b=c;  
+    }
+    return c;
+};
+int main(){
+cout<<fib(6);
+    return 0;
+}
