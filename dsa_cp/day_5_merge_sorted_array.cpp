3bedd4f0cfd84f85dc42ceea6c8d37335eeb18e8
croakie-ai <yugal2316mahajan@gmail.com>
2026-07-13 22:47:01 +0530

    day 5 dsa

---
+++ dsa_cp/day_5_merge_sorted_array.cpp
@@ -0,0 +1,30 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+class Solution{
+    void merge(vector<int>& nums1, int m, vector<int> &nums2, int n){
+        int last=m+n-1;
+        
+        while(m>0&n>0){
+            if(nums1[m-1]<nums2[n-1]){
+                nums1[last]=nums2[n-1];
+                n--;
+            }
+            else{
+                nums1[last]=nums2[n-1];
+                m--;
+            }
+            last--;
+        }
+        while(n>0){
+            nums1[last]=nums2[n];
+            last--;
+            n--;
+        }
+    }
+};
+
+int main(){
+    return 0;
+}
