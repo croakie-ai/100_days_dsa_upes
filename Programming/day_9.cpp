1476a154368456acfa8d0bd8b8de2c6c09b681a3
croakie-ai <yugal2316mahajan@gmail.com>
2026-07-14 23:55:49 +0530

    day_9_programming

---
+++ Programming/day_9.cpp
@@ -0,0 +1,29 @@
+// MIRRORING THE STRING PRINTING OUT IT REVERSED
+
+#include<vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left=0;
+        int right=s.size()-1;
+
+        while(left<=right){
+            swap(s[left],s[right]);
+            left++;
+            right--;
+        }
+    }
+};
+
+int main(){
+    Solution obj;
+    vector<char> s={'H','E','L','L','O'};
+    obj.reverseString(s);
+    for(auto i : s){
+        cout<<i;
+    }
+    return 0;
+}
